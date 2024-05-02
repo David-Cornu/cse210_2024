a8236dc3d2c6f38d13f2d353a146b839a032621c
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2024-05-02 11:37:00 -0600

    completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int gradePercent = 0;
+        string userInput = "";
+
+        Console.WriteLine("What is your grade percent?");
+        userInput = Console.ReadLine();
+        gradePercent = int.Parse(userInput);
+        Console.WriteLine();
+
+        if (gradePercent == 90 || gradePercent > 90)
+        {
+            Console.WriteLine("Your grade is A.");
+        } 
+        else if (gradePercent == 80 || gradePercent >80)
+        {
+            Console.WriteLine("Your grade is B.");
+        }
+        else if (gradePercent == 70 || gradePercent > 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (gradePercent == 60 || gradePercent >60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade id F");
+        }
     }
 }
