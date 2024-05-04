196d1380ede4753bde129eccb8454d5087cd8778
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2024-05-04 10:21:42 -0600

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userInput = "";
+        int userNumb = -1;
+        int biggest = -10000000;
+        int sum = 0;
+        float avg = 0;
+
+        List<int> numb = new List<int>();
+
+        while (userNumb != 0)
+        {
+            Console.WriteLine("Enter a list number of numbers. Enter 0 when finished");
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            userNumb = int.Parse(userInput);
+            if (userNumb != 0)
+            {
+                numb.Add(userNumb);
+            }
+        }
+        
+        foreach (int number in numb)
+        {
+            sum += number;
+        }
+
+        foreach (int number in numb)
+        {
+            avg = sum/numb.Count;
+        }
+
+        foreach (int number in numb)
+        {
+            if (number > biggest)
+            {
+                biggest = number;
+            }
+        }
+
+        Console.WriteLine($"This is the sum of your numbers: {sum}");
+        Console.WriteLine($"This is the average of your numbers: {avg}");
+        Console.WriteLine($"This is the largest number: {biggest}");
     }
 }
