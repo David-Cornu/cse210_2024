69a8e4e759770e71cd03eaa0cf90f936861e1576
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2024-05-04 10:48:06 -0600

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        
+        string name = GetName();
+        int userNumb = GetNumber();
+        int squa = Square(userNumb);
+        
+        DisplayResult(name, squa);
+  
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string GetName()
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            
+            return name;
+        }
+
+        static int GetNumber()
+        {
+            Console.Write("What is your favorite number: ");
+            int usernumb = int.Parse(Console.ReadLine());
+
+            return usernumb;
+        }
+
+        static int Square(int userNumb)
+        {
+            int squa = userNumb * userNumb;
+
+            return squa;
+        }
+
+        static void DisplayResult(string name, int squa)
+        {
+            Console.WriteLine($"{name} you number squared is {squa}.");
+        }
     }
 }
